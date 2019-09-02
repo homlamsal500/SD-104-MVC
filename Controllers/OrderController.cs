e389776dcbb261b6fbd24ad19a3562ee19433945
Hom Lamsal <hom.geek005@gmail.com>
2019-09-02 11:50:08 -0500

    c

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Order()
+        {
+            return View();
+        }
+        public IActionResult OrderCancel()
+        {
+            return View();
+
+        }
+    }
+}
